cc12b1fd48b7b69deb23fa068b64f8a26cbeb627
zhanjh <zhanjh@126.com>
2017-01-24 11:32:25 +0800

    Update ubuntu-dotconfig-cn.sh

--- ubuntu-dotconfig-cn.sh
+++ ubuntu-dotconfig-cn.sh
@@ -5,8 +5,40 @@
 
 #set -ex
 
-. ./lib/is_installed.sh
-. ./ubuntu/locale.sh
+function is_installed {
+	dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed"
+}
+
+locale_file="/etc/default/locale"
+profile_file="$HOME/.profile"
+
+if !  grep -Fq "LANG=" "$locale_file" ; then
+	echo 'LANG="en_US.UTF-8"' | sudo tee -a $locale_file > /dev/null
+	echo "$locale_file: LANG=\"en_US.UTF-8\""
+fi
+
+if !  grep -Fq "LANGUAGE=" "$locale_file" ; then
+	echo 'LANGUAGE="en_US.UTF-8"' | sudo tee -a "$locale_file" > /dev/null
+	echo "$locale_file: LANGUAGE=\"en_US.UTF-8\""
+fi
+
+if !  grep -Fq "LC_ALL=" "$locale_file" ; then
+	echo 'LC_ALL="en_US.UTF-8"' | sudo tee -a "$locale_file" > /dev/null
+	echo "$locale_file: LC_ALL=\"en_US.UTF-8\""
+fi
+
+if [ -z "$LC_CTYPE" ]; then
+	echo 'export LC_CTYPE=en_US.UTF-8' >> "$profile_file"
+	export LC_CTYPE=en_US=UTF-8
+	echo "$profile_file: LC_CTYPE=en_US.UTF-8"
+fi
+
+if [ -z "$LC_ALL" ]; then
+	echo 'export LC_ALL=en_US.UTF-8' >> "$profile_file"
+	export LC_ALL=en_US=UTF-8
+	echo "$profile_file: LC_ALL=en_US.UTF-8"
+fi
+
 
 if [ $(is_installed neovim) -eq 0 ]; then
     sudo add-apt-repository ppa:neovim-ppa/unstable
